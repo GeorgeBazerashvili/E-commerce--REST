bf5c28943f0de88f6fa6137845bebffa2f384c65
George Bazerashvili <bazerashviligeorge2000@gmail.com>
2023-09-29 12:50:44 +0400

    update

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 mongoose
-  .connect(process.env.CONNECTION_STRING)
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("connected to the database...");
   })
